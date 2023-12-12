00a78dab2dfa31c693207677f95f95a0041e354d
Mizuha-hk <hk.haru0803@gmail.com>
2023-12-12 21:26:08 +0900

    [add] request & response model

---
+++ api/Models/TodoModel.cs
@@ -0,0 +1,10 @@
+namespace api.Models
+{
+    public class TodoModel : TodoRequestModel
+    {
+        public string Id { get; set; }
+        // public string UserId { get; set; }
+        // public string Title { get; set; }
+        // public string Description { get; set; }
+    }
+}
---
+++ api/Models/TodoRequestModel.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class TodoRequestModel
+    {
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
